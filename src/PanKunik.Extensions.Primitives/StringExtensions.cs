f358bed2c40e817262b0803f59896833dac657b4
PanKunik <patryk.kunicki@pm.me>
2026-01-24 20:24:11 +0100

    Adding boolean 'IsNull()' extensions

--- src/PanKunik.Extensions.Primitives/StringExtensions.cs
+++ src/PanKunik.Extensions.Primitives/StringExtensions.cs
@@ -2,21 +2,24 @@ namespace PanKunik.Extensions.Primitives;
 
 public static class StringExtensions
 {
-    public static bool IsNull(this string? value)
-        => value is null;
-    
-    public static bool IsNotNull(this string? value)
-        => value is not null;
-    
-    public static bool IsEmpty(this string? value)
-        => string.IsNullOrEmpty(value);
-    
-    public static bool IsNotEmpty(this string? value)
-        => !string.IsNullOrEmpty(value);
-    
-    public static bool IsNullOrWhiteSpace(this string? value)
-        => string.IsNullOrWhiteSpace(value);
-    
-    public static bool IsNotNullOrWhiteSpace(this string? value)
-        => !string.IsNullOrWhiteSpace(value);
+    extension(string? value)
+    {
+        public bool IsNull()
+            => value is null;
+
+        public bool IsNotNull()
+            => !value.IsNull();
+
+        public bool IsEmpty()
+            => string.IsNullOrEmpty(value);
+
+        public bool IsNotEmpty()
+            => !value.IsEmpty();
+
+        public bool IsNullOrWhiteSpace()
+            => string.IsNullOrWhiteSpace(value);
+
+        public bool IsNotNullOrWhiteSpace()
+            => !value.IsNullOrWhiteSpace();
+    }
 }
